7bb18f8a327d898258fc5f53a8e1605181faf8f3
Daniel Capilla <100380211+Dacapi89@users.noreply.github.com>
2022-11-24 18:54:09 +0100

    Clase Bola, separada del script
    La clase bola, guarda toda la información de ella. Las colisiones las realiza la escena. Falta que haya fricción entre el suelo.

---
+++ Balloon_World_Championship/js/Bola.js
@@ -0,0 +1,28 @@
+ class Bola {
+
+    constructor(scene, player, plataforms) {
+        this.escena = scene;
+        this.jugador = player
+        this.plataformas = plataforms;
+        this.bolas = this.escena.physics.add.group(); //Añadimos físicas al grupo de las bolas
+        console.log("Bola creada.");
+    }
+    getBola() { return this.bola}
+
+    preload() {
+        this.escena.load.image('bomb', 'assets/bomb.png');
+    }
+    create() {
+        this.bola = this.bolas.create(200, 300, 'bomb'); //Creamos una bola
+        this.bola.body.setGravityY(300)
+        this.bola.setBounce(0.8);
+        this.bola.setCollideWorldBounds(true);
+        //this.escena.physics.add.collider(this.bola, this.plataformas);
+        //this.escena.physics.add.collider(this.jugador, this.bola, golpearBola(), null, this);
+    }
+    golpearBola() {
+        //bola.setVelocity(10, -100);
+        console.log("colision");
+    }
+}
+export {Bola};
