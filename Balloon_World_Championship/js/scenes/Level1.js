e35d0c4e58d425c44922d5599f4cbf8f24750f66
DanWfram <100808263+DanWfram@users.noreply.github.com>
2022-11-26 23:09:20 +0100

    fondo des-interpolado

--- Balloon_World_Championship/js/scenes/Level1.js
+++ Balloon_World_Championship/js/scenes/Level1.js
@@ -18,7 +18,6 @@ class Level1 extends Phaser.Scene{
         this.load.image("tiles","assets/images/background/tilemaps/tileset.png")
         this.load.tilemapTiledJSON("leveln1","levels/level1.json")          
         //players
-        this
         //ball
 
 
@@ -27,25 +26,26 @@ class Level1 extends Phaser.Scene{
     {
         //background
         this.add.image(0,0,"backgroundSky").setOrigin(0,0).setScrollFactor(0,0)
-        this.bg0=this.add.tileSprite(0,0,320,240,'backgroundAtmosphere').setOrigin(0,0).setScrollFactor(0,0)
-        this.bg1=this.add.tileSprite(0,0,320,240,'backgroundMountains').setOrigin(0,0).setScrollFactor(0,0)
-        this.bg2=this.add.tileSprite(0,0,320,240,'backgroundClouds').setOrigin(0,0).setScrollFactor(0,0)
-        this.bg3=this.add.tileSprite(0,0,320,240,'backgroundStars').setOrigin(0,0).setScrollFactor(0,0)
+        this.bg0=this.add.tileSprite(0,0,320,200,'backgroundAtmosphere').setOrigin(0,0).setScrollFactor(0,0)
+        this.bg1=this.add.tileSprite(0,0,320,200,'backgroundMountains').setOrigin(0,0).setScrollFactor(0,0)
+        this.bg2=this.add.tileSprite(0,0,320,200,'backgroundClouds').setOrigin(0,0).setScrollFactor(0,0)
+        this.bg3=this.add.tileSprite(0,0,320,200,'backgroundStars').setOrigin(0,0).setScrollFactor(0,0)
         //tiles
         const map = this.make.tilemap({ key : "leveln1" })
         this.tileset = map.addTilesetImage("tileSet", "tiles")
         this.tileLayer= map.createLayer("Map",this.tileset)
         this.tileLayer.setCollisionByProperty({collision: true})
         //players (setCollideWorldBorder,setvelocity, keypress,anims, etc)
-
-        
+        //ball 
     }
     update()
     {      
         //background     
-        //this.bg3.tilePositionX-=0.05;
+        this.bg3.tilePositionX-=0.05;
         this.bg2.tilePositionX-=0.2;
         this.bg1.tilePositionX-=0.5;
         this.bg0.tilePositionX-=1;
+        //player
+        //ball
     }
 }
