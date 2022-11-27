9af6c0514d8522988cd03f9c3e66fbea191169a0
Daniel Capilla <100380211+Dacapi89@users.noreply.github.com>
2022-11-27 19:44:30 +0100

    Level 1 con la Bola

--- Balloon_World_Championship/js/scenes/Level1.js
+++ Balloon_World_Championship/js/scenes/Level1.js
@@ -1,10 +1,11 @@
-
+import { Bola} from '../objects/Bola.js';
 function jump(object){
 
 }
 class Level1 extends Phaser.Scene{
     constructor(){
         super(Level1)
+        this.globo;
     }
     preload()
     {    
@@ -18,8 +19,11 @@ class Level1 extends Phaser.Scene{
         this.load.image("tiles","assets/images/background/tilemaps/tileset.png")
         this.load.tilemapTiledJSON("leveln1","levels/level1.json")          
         //players
+        
         //ball
+        this.globo = new Bola(this);
 
+        
 
     }
     create()
@@ -35,8 +39,32 @@ class Level1 extends Phaser.Scene{
         this.tileset = map.addTilesetImage("tileSet", "tiles")
         this.tileLayer= map.createLayer("Map",this.tileset)
         this.tileLayer.setCollisionByProperty({collision: true})
+        //this.tileLayer = this.physics.add.staticGroup(); NECESITO LA PROPIEDAD BODY!!!!!!!!
         //players (setCollideWorldBorder,setvelocity, keypress,anims, etc)
-        //ball 
+
+        //ball
+         this.globo.create();
+
+        //Collisions
+        this.physics.add.collider(this.globo.getBola(), this.tileLayer, function (globo, suelo) {
+            if(suelo.body.touching.up && globo.body.touching.down) { //Con el body lo que hago es que cuando la parte del globo de abajo toca la parte del cuerpo de las plataformas, entra en el if
+                this.globo.disableBody(true, true);
+                this.globo.enableBody(true, 200, 300, true, true);
+                console.log("HAS PERDIDO!!!!");
+            }
+        }); //Colisiones entre el globo y el suelo. Si el globo toca el suelo, se vuelve a lanzar el globo.
+
+
+        this.physics.add.collider(
+            player,
+            this.globo.getBola(),
+            function (_player, globo)
+            {
+                if (_player.body.touching.up && globo.body.touching.down)
+                {
+                    globo.setVelocity(_player.body.velocity.x,-200);
+                }
+            }); //Colision con el player en función de su velocidad REFERENCIA: https://phaser.io/examples/v3/view/physics/arcade/collision-direction#
     }
     update()
     {      
@@ -49,3 +77,4 @@ class Level1 extends Phaser.Scene{
         //ball
     }
 }
+export { Level1 };
